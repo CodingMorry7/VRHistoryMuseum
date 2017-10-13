ef70fb1d91952403736a40b6f08fb987ab0a7558
Brian  Dejesus <briandejesus@Lilys-iphone.csusm.edu>
2017-10-13 11:04:18 -0700

    Edit camera Brian's level

---
+++ Assets/Scripts/Egyptian_Level/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public static Camera mainCam;
+	// Use this for initialization
+	void Start () {
+		mainCam = Camera.main;
+		mainCam.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mainCam.enabled = true;
+	}
+}
