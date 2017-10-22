4cba68a96280d387084a7a4c7fd7e4b4281ddd1a
Brian  Dejesus <briandejesus@Brians-MBP.attlocal.net>
2017-10-22 14:57:33 -0700

    Add mummy spawner

---
+++ Assets/Scripts/Egyptian_Scripts/Mummy_Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mummy_Spawner : MonoBehaviour {
+
+	public GameObject mummy;
+	public float spawnTime = 4f;
+	public Transform[] spawnpoints;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+		int spawnIndex = Random.Range (0, spawnpoints.Length);
+
+		Instantiate (mummy, spawnpoints [spawnIndex].position, spawnpoints [spawnIndex].rotation);
+	}
+}
