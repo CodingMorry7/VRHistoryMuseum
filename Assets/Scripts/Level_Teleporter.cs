ea5b9be059c0e4fcc342dec312bcfa829581a6a5
CodingMorry7 <morikawac74@gmail.com>
2017-11-19 17:47:10 -0800

    Latest Changes to change to different world

--- Assets/Scripts/Level_Teleporter.cs
+++ Assets/Scripts/Level_Teleporter.cs
@@ -10,18 +10,22 @@ public class Level_Teleporter : MonoBehaviour {
 	public float RayLength = 30f;
 	// Use this for initialization
 	public string LevelName;
-
+	public AudioSource TeleportLevelSound;
 
 	// Update is called once per frame
 	void Update () {
 		Ray ray = new Ray(transform.position, transform.forward);
 		RaycastHit hit;
 		if(Physics.Raycast(ray, out hit, RayLength)) {
-			if(hit.collider.tag == "EgyptLevel") {
+			if(hit.collider.tag == "EgyptLevel" ) {
 				if(!Teleport_Lvl_Mark.activeSelf)  {
 					Teleport_Lvl_Mark.SetActive(true);
 				}
 				Teleport_Lvl_Mark.transform.position = hit.point;
+				if(OVRInput.GetDown(OVRInput.Button.One)){
+					//TeleportLevelSound.Play();
+					Application.LoadLevel(LevelName);
+				}
 			}
 			else {
 				Teleport_Lvl_Mark.SetActive(false);
@@ -31,9 +35,9 @@ public class Level_Teleporter : MonoBehaviour {
 			Teleport_Lvl_Mark.SetActive(false);
 		}
 
-		if(OVRInput.GetDown(OVRInput.Button.One)) {
-			Application.LoadLevel(LevelName);
-		}
+		//if(OVRInput.GetDown(OVRInput.Button.Two) ) {
+			//Application.LoadLevel(LevelName);
+		//}
 	}
 
 }
