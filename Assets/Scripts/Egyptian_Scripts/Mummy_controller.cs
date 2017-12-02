47adc471800ab30b34005faa6bfb0eaa369dfd25
Brian  Dejesus <briandejesus@Brians-MacBook-Pro.local>
2017-12-02 12:03:12 -0800

    Update mummy controller script

--- Assets/Scripts/Egyptian_Scripts/Mummy_controller.cs
+++ Assets/Scripts/Egyptian_Scripts/Mummy_controller.cs
@@ -8,11 +8,25 @@ public class Mummy_controller : Axe {
 	/*private string _lookInStumik, _walk;*/
 	private NavMeshAgent mummy;
 	public Transform target;
+	private int health = 100;
 
 	protected override void BaseButtons ()
 	{
 		//Hide the buttons
 	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.CompareTag("Sword")) {
+			health -= 50;
+		}
+	}
+
+	IEnumerator Wait()
+	 {
+			 print(Time.time);
+			 yield return new WaitForSeconds(3);
+			 print(Time.time);
+	 }
 
 	void Start () {
 		_anim.SetTrigger (_runTr);
@@ -27,8 +41,15 @@ public class Mummy_controller : Axe {
 		if (mummy.remainingDistance < 1.0) {
 			_anim.SetTrigger (_atack_1);
 		}
+
+		if (health <= 0) {
+			_anim.SetTrigger (_dieTr);
+			Wait();
+			Destroy(mummy);
+		}
+
 	}
-		
+
 
 
 }
