1ad9a9888895d5e070158d3487dfa0d0c51c1467
CodingMorry7 <morikawac74@gmail.com>
2017-12-13 21:32:25 -0800

    Updated the Medieval Map, The Arrow sticks to the Target, also make a sound effect when hit, and Music to the Medieval map.

--- Assets/HitByArrow.cs
+++ Assets/HitByArrow.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HitByArrow : MonoBehaviour {
-//public Audiosource SoundEffect;
-void OnTriggerEnter(Collider other) {
-	if(other.gameObject.CompareTag("Target")){
-		//SoundEffect.Play();
-		other.gameObject.SetActive(false);
+public float waitTime;
+ public AudioSource SoundEffect;
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Arrow")){
+			SoundEffect.Play();
+			other.attachedRigidbody.useGravity = false;
+			other.attachedRigidbody.isKinematic = true;
+		}
+		float time = 0f;
+		while(time != waitTime){
+			time++;
+			if(time == waitTime){
+				Destroy(other.gameObject);
+			}
+		}
+
 	}
-}
 }
