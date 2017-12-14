3b7fcc33545daadaa6fbb09e285155bcc3deedaf
CodingMorry7 <morikawac74@gmail.com>
2017-12-14 12:40:12 -0800

    Created the Japan world to be interactive

---
+++ Assets/HitByStar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitByStar : MonoBehaviour {
+	 public AudioSource SoundEffect;
+		void OnTriggerEnter(Collider other) {
+			if(other.gameObject.CompareTag("NinjaStar")){
+				SoundEffect.Play();
+				other.attachedRigidbody.useGravity = false;
+				other.attachedRigidbody.isKinematic = true;
+			}
+		}
+}
---
+++ Assets/PlayHandSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayHandSound : MonoBehaviour {
+	public AudioSource SoundEffect;
+	 void OnTriggerEnter(Collider other) {
+		 if(other.gameObject.CompareTag("NinjaStar")){
+			 SoundEffect.Play();
+		 }
+	 }
+}
